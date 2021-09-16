338886de7f4288fab9fd564ced660a03f8ff1425
bennettj12 <bennettjohnson0@gmail.com>
2021-09-16 01:04:46 -0500

    added basic starting code for validateMove

--- app/src/main/java/chess/MoveValidator.java
+++ app/src/main/java/chess/MoveValidator.java
@@ -5,7 +5,36 @@ import java.util.ArrayList;
 public class MoveValidator {
     boolean validateMove(MoveIntent intent, int[] board, ArrayList<MoveIntent> moveRecord, PlayerColor playerColor) {
         //TODO: write method for move validation.
-
-        return false;
+        boolean moveIsLegal = false;
+        switch(intent.chessPiece){
+            case PAWN:
+                //TODO: handle pawn move validation
+                break;
+            case KNIGHT:
+                //TODO: handle knight move validation
+                break;
+            case BISHOP:
+                //TODO: handle bishop move validation
+                break;
+            case ROOK:
+                //TODO: handle rook move validation
+                break;
+            case QUEEN:
+                //TODO: handle queen move validation
+                break;
+            case KING:
+                //TODO: handle king move validation
+                break;
+            default:
+                //shouldn't happen
+                break;
+        }
+        return moveIsLegal;
+    }
+    private int getPiece(File file, Rank rank, int[] board){
+        return board[rank.value * 8 + file.value];
+    }
+    private int getPiece(int x, int y, int[] board) {
+        return board[y * 8 + x];
     }
 }
