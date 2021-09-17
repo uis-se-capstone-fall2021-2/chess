0f479791505f1ddd39f18fb727d7f74c142e8a3e
bennettj12 <49352128+bennettj12@users.noreply.github.com>
2021-09-16 23:20:34 -0500

    Update GameRecord.java

--- app/src/main/java/chess/GameRecord.java
+++ app/src/main/java/chess/GameRecord.java
@@ -6,12 +6,11 @@ import java.util.ArrayList;
  * How a game is persisted to db
  */
 public class GameRecord extends GameInfo{
-    int[] board;
     //movesRecord should hold verified MoveIntents in the order that the moves were made. index 0: White move 1, index 1: black move 1, index 2: white move 2...
     ArrayList<MoveIntent> movesRecord;
 
-    public GameRecord(int gameId, int owner, int winner, int[] players, long moveCount, GameCompletionState completed, int[] board, ArrayList<MoveIntent> movesRecord) {
+    public GameRecord(int gameId, int owner, int winner, int[] players, long moveCount, GameCompletionState completed, ArrayList<MoveIntent> movesRecord) {
         super(gameId, owner, winner, players, moveCount, completed);
-        this.board = board;
+        this.movesRecord = movesRecord;
     }
 }
