91a390b97d0d3c40d20166fff09e95df633f091c
bennettj12 <bennettjohnson0@gmail.com>
2021-10-03 15:58:44 -0500

    updated tests to work with changes made to board and movevalidator

--- app/src/test/java/chess/MoveValidationTest.java
+++ app/src/test/java/chess/MoveValidationTest.java
@@ -5,24 +5,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
+import chess.board.Board;
 
 public class MoveValidationTest {
     MoveValidator validator;
     ArrayList<MoveIntent> moveRecord;
-    int[] board;
+    Board board;
 
     @BeforeEach
     public void init() {
         validator = new MoveValidator();
         moveRecord = new ArrayList<MoveIntent>();
-        board = new int[] {
-            2,3,4,6,5,4,3,2,
-            1,1,1,1,1,1,1,1,
-            0,0,0,0,0,0,0,0,
-            0,0,0,0,0,0,0,0,
-            -1,-1,-1,-1,-1,-1,-1,-1,
-            -2,-3,-4,-6,-5,-4,-3,-2
-        };
+        board = new Board();
         
     }
 
@@ -30,31 +24,31 @@ public class MoveValidationTest {
     @Test void testLegalKnightMove() {
         Position currentPosition = new Position(File.FromInteger(1), Rank.FromInteger(0)); // knight at B1
         Position desiredPosition = new Position(File.FromInteger(2), Rank.FromInteger(2)); // Knight at C3
-        ChessPiece piece = ChessPiece.FromInteger(Math.abs(validator.getPiece(currentPosition,board)));
-        assertTrue(validator.validateMove(new MoveIntent(piece, currentPosition, desiredPosition), board, moveRecord, PlayerColor.WHITE));
+        ChessPiece piece = ChessPiece.FromInteger(board.getPiece(currentPosition));
+        assertTrue(validator.validateMove(new MoveIntent(piece, currentPosition, desiredPosition), board, moveRecord));
     }
     @DisplayName("Knight should be able to jump on opponent")
     @Test void testLegalKnightCapture() {
         Position currentPosition = new Position(File.FromInteger(1), Rank.FromInteger(0)); // knight at B1
         Position desiredPosition = new Position(File.FromInteger(2), Rank.FromInteger(2)); // Knight at C3
-        board[18] = -1; // put an enemy pawn blocking the desired spot, C3
-        ChessPiece piece = ChessPiece.FromInteger(Math.abs(validator.getPiece(currentPosition,board)));
-        assertTrue(validator.validateMove(new MoveIntent(piece, currentPosition, desiredPosition), board, moveRecord, PlayerColor.WHITE));
+        board.board[18] = -1; // put an enemy pawn blocking the desired spot, C3
+        ChessPiece piece = ChessPiece.FromInteger(board.getPiece(currentPosition));
+        assertTrue(validator.validateMove(new MoveIntent(piece, currentPosition, desiredPosition), board, moveRecord));
     }
     @DisplayName("Knight should not be able to jump on it's own teamate")
     @Test void testIllegalKnightMove() {
         Position currentPosition = new Position(File.FromInteger(1), Rank.FromInteger(0)); // knight at B1
         Position desiredPosition = new Position(File.FromInteger(2), Rank.FromInteger(2)); // Knight at C3
-        board[18] = 1; // put a pawn blocking the desired spot, C3
-        ChessPiece piece = ChessPiece.FromInteger(Math.abs(validator.getPiece(currentPosition,board)));
-        assertFalse(validator.validateMove(new MoveIntent(piece, currentPosition, desiredPosition), board, moveRecord, PlayerColor.WHITE));
+        board.board[18] = 1; // put a pawn blocking the desired spot, C3
+        ChessPiece piece = ChessPiece.FromInteger(board.getPiece(currentPosition));
+        assertFalse(validator.validateMove(new MoveIntent(piece, currentPosition, desiredPosition), board, moveRecord));
     }
     @DisplayName("Knight should not be able to jump out of bounds")
     @Test void testOutOfBoundsKnightMove() {
-        board[16] = 3; // Knight at A3
+        board.board[16] = 3; // Knight at A3
         Position currentPosition = new Position(File.FromInteger(0), Rank.FromInteger(2)); // knight at B1
         Position desiredPosition = new Position(File.FromInteger(-1), Rank.FromInteger(4)); // Knight at C3
-        ChessPiece piece = ChessPiece.FromInteger(Math.abs(validator.getPiece(currentPosition,board)));
-        assertFalse(validator.validateMove(new MoveIntent(piece, currentPosition, desiredPosition), board, moveRecord, PlayerColor.WHITE));
+        ChessPiece piece = ChessPiece.FromInteger(board.getPiece(currentPosition));
+        assertFalse(validator.validateMove(new MoveIntent(piece, currentPosition, desiredPosition), board, moveRecord));
     }
 }
