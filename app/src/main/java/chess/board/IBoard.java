813d307785819dbd7f8c389cda29bc323254e03b
bennettj12 <bennettjohnson0@gmail.com>
2021-10-03 14:57:06 -0500

    Basic board implementation, needed this for moveValidator to function

--- app/src/main/java/chess/board/IBoard.java
+++ app/src/main/java/chess/board/IBoard.java
@@ -1,11 +1,10 @@
 package chess.board;
 
-import chess.ChessPiece;
 import chess.MoveIntent;
 import chess.Position;
 
 public interface IBoard {
-  public ChessPiece getPiece(Position position);
+  public int getPiece(Position position);
   public int[] updateBoard(MoveIntent intent);
 }
   
