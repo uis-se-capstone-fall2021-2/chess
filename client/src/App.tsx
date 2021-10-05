f9893c46eeb3dbfdfeb487698ff5df0584f368f8
Ethan Ferrari <github@ethanferrari.com>
2021-10-04 20:04:11 -0500

    add material ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {Auth0Provider, Auth0Context, Auth0ContextInterface, User} from '@auth0/auth0-react';
 import {LoginButton} from './user/components/LoginButton';
 import {LogoutButton} from './user/components/LogoutButton';
+import {AppBar, Box, Button, Container, IconButton, Toolbar, Typography} from '@mui/material';
+import {Menu as MenuIcon} from '@mui/icons-material';
 
 export default class App extends React.Component<{}, {token: string|null}> {
 
@@ -19,16 +21,30 @@ export default class App extends React.Component<{}, {token: string|null}> {
         
           <Auth0Context.Consumer>
             {(user: Auth0ContextInterface) => (
-              <div className='app-container'>
-                <div className='app-header'>
+              <Box sx={{flexGrow: 1}}>
+                <AppBar>
+                <Toolbar>
+                  <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Chess
+                  </Typography>
                   {user.isAuthenticated ? <LogoutButton/> : <LoginButton/>}
-                </div>
-                <div className='app-body'>
+                </Toolbar>
+                </AppBar>
+                <Container>
                   <pre>{JSON.stringify(user, null, 2)}</pre>
-                  <button onClick={() => this.getToken(user)}>Get Token</button>
+                  <Button onClick={() => this.getToken(user)}>Get Token</Button>
                   <span>{`Token: ${this.state.token}`}</span>
-                </div>
-              </div>
+                </Container>
+              </Box>
             )}
           </Auth0Context.Consumer>
        
--- client/src/user/components/LoginButton.tsx
+++ client/src/user/components/LoginButton.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import {useAuth0} from "@auth0/auth0-react";
+import {Button} from '@mui/material';
 
 export function LoginButton() {
   const {loginWithRedirect} = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <Button color='inherit' onClick={() => loginWithRedirect()}>Log In</Button>;
 };
