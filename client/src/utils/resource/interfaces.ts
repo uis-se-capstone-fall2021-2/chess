1833dfdd85213b7ca966b6c53cbc3fcd1e12ce77
gleiva2 <leivag22@gmail.com>
2021-11-14 13:03:27 -0600

    added patch method to Resource  interfaces

--- client/src/utils/resource/interfaces.ts
+++ client/src/utils/resource/interfaces.ts
@@ -12,6 +12,7 @@ export interface Resource {
   get<T>(path: string): Promise<T>;
   post<T>(path: string, body: object): Promise<T>;
   put<T>(path: string, body: object): Promise<T>;
+  patch<T>(path: string, body: object): Promise<T>;
   delete(path: string): Promise<void>;
 }
 
