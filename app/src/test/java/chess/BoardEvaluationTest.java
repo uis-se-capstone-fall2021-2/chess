fccd0d6d5a52d10c8f0dc1065e7a720048afc9da
Bennett Johnson <bennettjohnson0@gmail.com>
2021-11-30 13:03:51 -0600

    more board evaluation tests

--- app/src/test/java/chess/BoardEvaluationTest.java
+++ app/src/test/java/chess/BoardEvaluationTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import chess.ai.model.ChessAI;
 import chess.board.Board;
 
+
+
 public class BoardEvaluationTest {
     Board board;
 
@@ -38,5 +40,32 @@ public class BoardEvaluationTest {
         }
         int boardScore = ChessAI.getBoardScore(board);
         assertEquals(0, boardScore);
+    }
+
+    @DisplayName("Black opened with a poor pawn move, so white should have a better score")
+    @Test void checkWhiteFavoredEvaluation() {
+
+        MoveIntent[] moves = {
+            ChessTestUtilities.stringToMoveIntent("e2 e4", board),
+            ChessTestUtilities.stringToMoveIntent("h7 h6", board),
+        };
+        for(int i = 0; i < moves.length; i++) {
+            board.updateBoard(moves[i]);
+        }
+        int boardScore = ChessAI.getBoardScore(board);
+        assertEquals(40, boardScore);
+    }
+    @DisplayName("White opened with a poor pawn move, so black should have a better score")
+    @Test void checkBlackFavoredEvaluation() {
+
+        MoveIntent[] moves = {
+            ChessTestUtilities.stringToMoveIntent("h2 h3", board),
+            ChessTestUtilities.stringToMoveIntent("d7 d5", board),
+        };
+        for(int i = 0; i < moves.length; i++) {
+            board.updateBoard(moves[i]);
+        }
+        int boardScore = ChessAI.getBoardScore(board);
+        assertEquals(-40, boardScore);
     }
 }
